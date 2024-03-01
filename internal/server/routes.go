06b5d44b8b2a79213cdc68207219871b6f21d141
JAKHONGIR <jakhon99dev@gmail.com>
2024-03-02 00:46:36 +0500

    v.0

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -11,7 +11,7 @@ func (s *FiberServer) RegisterFiberRoutes() {
 
 func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
 	resp := fiber.Map{
-		"message": "Assalomu alaykum hammaga! Nihoyat!",
+		"message": "Assalomu alaykum hammaga! Nihoyat! Ishlarda",
 	}
 
 	return c.JSON(resp)
